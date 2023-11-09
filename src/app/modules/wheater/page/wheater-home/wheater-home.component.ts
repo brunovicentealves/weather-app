0db84bd1e1bf50c3b40436fc063a6967afeff52c
Bruno Vicente Alves <brunovicentealves.ti@gmail.com>
2023-11-09 19:07:35 -0300

    adjustment component home wheather

--- src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
+++ src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,OnDestroy } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/weather.interface';
+import {Subject,takeUntil } from 'rxjs'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-wheater-home',
   templateUrl: './wheater-home.component.html',
   styleUrls: []
 })
-export class WheaterHomeComponent {
+export class WheaterHomeComponent implements OnInit , OnDestroy{
+
+private readonly destroy$:Subject<void>=new Subject();
+
+initialCityName:string='Porto Alegre';
+
+weatherDatas!: WeatherDatas;
+
+searchIcon=faMagnifyingGlass
+
+constructor(private weatherService:WeatherService){}
+
+
+ngOnInit(): void {
+    this.getweatherDatas(this.initialCityName);
+}
+
+ getweatherDatas(cityName:string):void{
+
+  this.weatherService.getweatherDatas(cityName)
+  .pipe(
+    takeUntil(this.destroy$)
+  )
+  .subscribe({
+    next: (response)=>{
+     response && (this.weatherDatas=response);
+     console.log(this.weatherDatas)
+    },
+    error:(error)=>console.log(error),
+
+  })
+}
+
+onSubimit():void{
+this.getweatherDatas(this.initialCityName);
+this.initialCityName='';
+}
+
+ngOnDestroy(): void {
+this.destroy$.next();
+this.destroy$.complete();
+}
+
 
 }

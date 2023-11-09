9874d3c6c592a25a2cf5393ee6b283e66f422b34
Bruno Vicente Alves <brunovicentealves.ti@gmail.com>
2023-11-09 19:05:36 -0300

    create service

--- src/app/modules/wheater/services/weather.service.ts
+++ src/app/modules/wheater/services/weather.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+ private apikey = '3a929a8e387d753611a87759f226c2f9'
+  constructor(private http:HttpClient ) { }
 
-  constructor() { }
+
+  getweatherDatas(city:String): Observable<any>{
+
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&mode=json&appid=${this.apikey}`,{})
+  }
 }
